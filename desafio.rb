6f8251271b2e7f9796c88502413e22bcb96674a9
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2024-05-06 20:58:25 -0300

    first commit

---
+++ desafio.rb
@@ -0,0 +1,13 @@
+puts "Desafio Biblioteca"
+puts "=========================================================================="
+
+puts "Digite o seu nome: "
+nome = gets.chomp
+
+puts "Digite o seu sobrenome: "
+sobrenome = gets.chomp
+
+puts "Digite a sua idade? "
+idade = gets.chomp
+
+puts "Olá #{nome} #{sobrenome} e você tem #{idade} anos de vida!"
